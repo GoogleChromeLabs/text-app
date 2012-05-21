9794d1efa2471ac8bf0a6eeadfc0700f45ff9c34
Vojta Jina <vojta.jina@gmail.com>
2012-05-21 15:00:47 -0700

    Very simple editor

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,20 @@
-angular.module('TD', []);
+var app = angular.module('TD', []);
+
+app.factory('editor', function() {
+  var editor = ace.edit('editor');
+  var EditSession = ace.require("ace/edit_session").EditSession;
+  var INIT_CONTENT = "function reverseString(str) {\n" +
+                     "  return str.split('').reverse().join('');\n" +
+                     "}\n";
+
+  editor.setSession(new EditSession(INIT_CONTENT));
+  editor.setTheme("ace/theme/twilight");
+  editor.getSession().setMode("ace/mode/javascript");
+
+  return editor;
+});
+
+
+app.run(function(editor) {
+  editor.focus();
+});
