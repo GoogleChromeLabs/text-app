dec12ed645d5905688e263a81a8d6ac9b8699e8b
Vojta Jina <vojta.jina@gmail.com>
2012-06-21 10:47:40 -0700

    Remove some console.log() calls

--- app/js/directives/tdWidth.js
+++ app/js/directives/tdWidth.js
@@ -1,7 +1,6 @@
 TD.directive('tdWidth', function() {
   return function(scope, elm, attr) {
     scope.$watch(attr.tdWidth, function(width) {
-      console.log('WIDTH', width + 'px');
       elm.css('width', width + 'px');
     });
   };
