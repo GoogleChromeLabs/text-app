c72176798c72a4723a941ab236a6fdf4eb8d81bc
Vojta Jina <vojta.jina@gmail.com>
2012-06-21 10:47:06 -0700

    Change frame to 'none'

--- app/js/background.js
+++ app/js/background.js
@@ -1,11 +1,14 @@
-chrome.experimental.app.onLaunched.addListener(function () {
+chrome.experimental.app.onLaunched.addListener(function (launchData) {
   var options = {
-    frame: 'custom',
+    frame: 'none',
+    minWidth: 400,
+    minHeight: 400,
     width: 900,
     height: 900
   };
 
   chrome.appWindow.create('index.html', options, function (win) {
+    win.launchData = launchData;
 //    win.onload = function () {};
   });
 });
