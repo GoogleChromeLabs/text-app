6ce1c7fa1816cde4ebe39d1ce18b3c70dacbe577
Oleg Eterevsky <oleg@chromium.org>
2012-11-20 17:29:20 +0400

    Simplistic shell script for packaging. TODO: restore grunt, or configure some other building method.

---
+++ pack.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+TARGET=build/textdrive
+
+rm -rf $TARGET
+mkdir $TARGET
+cp -rv app/css app/icon app/js app/index.html app/manifest.json $TARGET
+mkdir $TARGET/lib
+cp -rv app/lib/jquery-1.8.3.js app/lib/font-awesome $TARGET/lib
+mkdir -p $TARGET/lib/ace/src-noconflict
+cp app/lib/ace/src-noconflict/ace.js $TARGET/lib/ace/src-nonconflict
