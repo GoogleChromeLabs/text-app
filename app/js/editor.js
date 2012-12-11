86d870fc09b21b4178455154d104ba1d9479676c
Oleg Eterevsky <oleg@eterevsky.com>
2012-12-11 17:32:53 +0400

    Tweak the CSS for the ace editor panel.
    Issue #13

--- app/js/editor.js
+++ app/js/editor.js
@@ -9,6 +9,7 @@ function Editor(editorElement) {
   this.editor_ = ace.edit(this.element_);
   this.editor_.on('change', this.onChange.bind(this));
   this.editor_.setShowPrintMargin(false);
+  this.editor_.setFontSize(20);
 }
 
 Editor.prototype.newSession = function(opt_content) {
