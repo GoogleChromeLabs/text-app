217649bd60cbe7554ed3ae7446ad0a427df10111
Oleg Eterevsky <oleg@chromium.org>
2014-03-13 18:50:51 +0400

    Make search count red in case no matches are found. Also hide the search count in case the search string is empty. Issue #42

--- js/controllers/search.js
+++ js/controllers/search.js
@@ -13,10 +13,19 @@ function SearchController(search) {
 }
 
 SearchController.prototype.showSearchCount_ = function() {
+  if ($('#search-input').val().length === 0) {
+    $('#search-counting').text('');
+    return;
+  }
   var searchCount = this.search_.getResultsCount();
   var searchIndex = this.search_.getCurrentIndex();
   $('#search-counting').text(chrome.i18n.getMessage('searchCounting',
       [searchIndex, searchCount]));
+  if (searchCount === 0) {
+    $('#search-counting').addClass('nomatches');
+  } else {
+    $('#search-counting').removeClass('nomatches');
+  }
 };
 
 SearchController.prototype.findNext_ = function(opt_reverse) {
