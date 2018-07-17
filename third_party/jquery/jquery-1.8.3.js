5edfadcc675cccb2673457c9a2c9ba6e8e348be3
BugsNash <BugsNash@users.noreply.github.com>
2018-07-17 14:43:00 +1000

    Move jquery into third_party. (#365)

--- third_party/jquery/jquery-1.8.3.js
+++ third_party/jquery/jquery-1.8.3.js
@@ -5,10 +5,6 @@
  * Includes Sizzle.js
  * http://sizzlejs.com/
  *
- * Copyright 2012 jQuery Foundation and other contributors
- * Released under the MIT license
- * http://jquery.org/license
- *
  * Date: Tue Nov 13 2012 08:20:33 GMT-0500 (Eastern Standard Time)
  */
 (function( window, undefined ) {
